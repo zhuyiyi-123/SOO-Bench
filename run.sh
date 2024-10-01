bf992791a84b667dfe657d013fd87b63c08a435d
yiyizhu <82349808+zhuyiyi-123@users.noreply.github.com>
2024-10-01 14:18:05 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash ./run_unconstraint.sh
+
+bash ./run_constraint.sh
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='soo_bench',
+    version='0.2.0', 
+    author='Zhu Yiyi', 
+    author_email='wyl576125697@126.com', 
+    description='Benchmarks for Evaluating the Stability of Offline Black-Box Optimization',  
+    long_description=open('README.md', encoding='utf-8').read(), 
+    long_description_content_type='text/markdown',  
+    url='https://github.com/zhuyiyi-123/SOO-Bench',  
+    py_modules=['soo_bench'],
+    packages=find_packages(), 
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', 
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.8', 
+    install_requires=[
+        'revive==0.7.3'
+    ],
+    
+)
