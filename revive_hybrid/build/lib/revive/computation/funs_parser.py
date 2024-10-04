dde0e1b677aabd356889cdbb307c0a8ef38ad8a5
yiyizhu <82349808+zhuyiyi-123@users.noreply.github.com>
2024-10-04 21:07:26 +0800

    Update funs_parser.py

--- revive_hybrid/build/lib/revive/computation/funs_parser.py
+++ revive_hybrid/build/lib/revive/computation/funs_parser.py
@@ -1,18 +1,3 @@
-''''''
-"""
-    POLIXIR REVIVE, copyright (C) 2021-2022 Polixir Technologies Co., Ltd., is 
-    distributed under the GNU Lesser General Public License (GNU LGPL). 
-    POLIXIR REVIVE is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 3 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-"""
-
 import re
 import yaml
 from shutil import copyfile
