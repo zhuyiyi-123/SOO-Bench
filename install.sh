192ae0c26374cf0febf40d7ead7f3ee10a549256
yiyizhu <82349808+zhuyiyi-123@users.noreply.github.com>
2025-03-02 14:09:55 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# # install soo-bench
+pip install -e .
+
+# # install dependencies of algorithms
+pip install -r requirements.txt
+
+# install all
+# pip install -e . -r requirements.txt
